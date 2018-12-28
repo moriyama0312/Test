b754fdb5f5c0e4cb5898d37aed68d031338a60a8
Moriyama Masataka <masataka.312@gmail.com>
2018-12-28 23:51:21 +0900

    browser-sync 追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
+var browser = require("browser-sync");
+
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task("sass", function(){
 	gulp.src("./resource/sass/*.scss")
