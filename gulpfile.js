31cfbc94c43a24834ca609e95fcb0d2ba976c3e6
Moriyama Masataka <masataka.312@gmail.com>
2018-12-28 23:25:03 +0900

    gulpfile sass 追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
 
 gulp.task("sass", function(){
 	gulp.src("./resource/sass/*.scss")
 		.pipe(sass())
-		.pipe(gulp.dest("./resource/css/"))
+		.pipe(gulp.dest("./resource/css/"));
 });
